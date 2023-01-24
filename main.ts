7bd9488a92afd25c155324e2cd86f49474d54faa
jesusElDeTic <74316857+jesusElDeTic@users.noreply.github.com>
2023-01-24 19:48:22 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,13 @@
-radio.setGroup(1)
-basic.forever(function () {
-	
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showString("Temperatura satélite" + receivedNumber)
 })
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Temperatura base" + input.temperature())
+})
+radio.onReceivedString(function (receivedString) {
+    radio.sendNumber(input.temperature())
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("temperatura satélite?")
+})
+radio.setGroup(1)
